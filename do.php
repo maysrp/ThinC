125fbbbd8305e04c9aef15a4516c051386454631
Xiaosea <maysrp@gmail.com>
2017-04-04 02:19:02 +0800

    Update do.php

--- do.php
+++ do.php
@@ -2,7 +2,7 @@
 	$command=isset($_GET['command'])?$_GET['command']:0;
 	$achieve=isset($_POST['achieve'])?$_POST['achieve']:0;
 	if($command){
-		@file_put_contents('./result.sh', $command);
+		@file_put_contents('./result.sh', $command." >achieve.md");//执行结果输入到achieve.md中
 	}
 	if($achieve==233){
 		$ach=@file_get_contents('./achieve.md');
